81be3826ab00dfa52d3ec06f58e0790e00024abc
unknown <yeasin18801@gmail.com>
2022-11-05 22:36:11 +0600

    payment-3

--- resources/views/back-end/page/student/student_payment.blade.php
+++ resources/views/back-end/page/student/student_payment.blade.php
@@ -4,21 +4,30 @@
 <form action="{{ route('student-create') }}" method="POST" enctype="multipart/form-data">@csrf
 <div class="row">
 
-      <div class="col-6 pt-2" style="margin-left: 20px">
+      <div class="col-12 pt-2" style="margin-left: 20px">
         <div class="card">
             <div class="card-body ml-2">
-              <h5 class="card-title">Add a new Student </h5>
-
-                <div class="form-group">
-                  <label for="formGroupExampleInput">Student's Name</label>
-                  <input type="text" class="form-control" id="formGroupExampleInput1" placeholder="Enter Student's Name" name="student_name">
-                </div>
-                <div class="form-group">
-                  <label for="formGroupExampleInput2">Student's Class</label>
-                  <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Class" name="student_class">
-                </div>
+              <h5 class="card-title">Student Payment</h5>
 
 
+
+              <div class="form-group">
+                <label for="formGroupExampleInput" class="mt-3">Month:</label>
+                <tr class="text-center">
+                    <td class="m-3" style="margin: 0 0 0 17px"><input  type="checkbox"  name="month[]" value="January">January</td>
+                    <td  class="m-3"><input type="checkbox"  name="month[]" value="February">February</td>
+                    <td  class="m-3"><input type="checkbox"  name="month[]" value="March">March</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="April">April</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="May">May</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="June">June</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="July">July</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="August">August</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="September">September</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="October">October</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="November">November</td>
+                    <td class="ml-3"><input type="checkbox"  name="month[]" value="December">December</td>
+                </tr>
+          </div>
 
 
 
@@ -26,20 +35,7 @@
             </div>
           </div>
       </div>
-      <div class="col-lg-4 pt-2">
-        <div class="card">
-            <div class="card-body">
 
-                <div class="form-group">
-                    <label for="formGroupExampleInput2">Student Email</label>
-                    <input type="email" class="form-control" id="formGroupExampleInput5" placeholder="Email" name="email">
-                  </div>
-
-
-            </div>
-        </div>
-
-    </div>
 
     <div style="margin-left: 21px; margin-buttom: 10px;">
         <button class="btn btn-primary mt-3 ml-3">Submit</button>
