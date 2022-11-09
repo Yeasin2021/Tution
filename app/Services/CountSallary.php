10809a31be7d345c3a8553d224825cb976769910
unknown <yeasin18801@gmail.com>
2022-11-09 09:32:53 +0600

    Facades-makes-6

--- app/Services/CountSallary.php
+++ app/Services/CountSallary.php
@@ -1,11 +1,15 @@
 <?php
 namespace App\Services;
+use App\Models\Student;
 
 
 class CountSallary{
 
     public function studentSallary(){
-        echo "Hello I am Working Bro !!!";
+        // echo "Hello I am Working Bro !!!";
+        $sallry = Student::all();
+        // dd($sallry->sum('tution_fee'));
+        return $sallry->sum('tution_fee');
     }
 
 
